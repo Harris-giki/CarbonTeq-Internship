5a32ac6f1fe21c7e9c6b28f208c762b9a7578cdd
harris-giki <harris-giki@gmail.com>
2025-06-30 13:31:06 +0500

    Search Component Added

--- React-Basics/MiniProject/practising-react/src/App.jsx
+++ React-Basics/MiniProject/practising-react/src/App.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import Search from "./components/search.jsx";
+import { useState } from "react";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
-    </div>
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <h1>
+            <img src="./hero.png" alt="Hero Banner" />
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            without a Hassle
+          </h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+    </main>
   );
 };
 
--- React-Basics/notes.md
+++ React-Basics/notes.md
@@ -8,12 +8,15 @@ prop can be anything
 3. numbers
 4. complex props etc
 
+-> Remember that props are never to be changed by the child components but rather read-only.
+
 **State Hooke**
 State is like react component's brain, it can hold information that can change over time.
 
 state change is not persistant across browser reload
 state is just another hooke
 
+-> only mutate the state using the setter function
 **hookes**
 hooke are basically special functions in react that let us tap into special features of react example:
 
