bf178e41b37b99f3d6b3e53fdf2e20d94f8ba01b
Harris-giki <harris.giki@gmail.com>
2025-07-01 12:57:34 +0500

    Files renamed

---
+++ React-Basics/MiniProject/JS-Version/src/components/Spinner.jsx
@@ -0,0 +1,19 @@
+// Spinner.jsx
+import * as React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+
+export default function Spinner() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "200px",
+      }}
+    >
+      <CircularProgress color="secondary" />
+    </Box>
+  );
+}
